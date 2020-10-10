9f9fb7e9f12a7e76997134bfb554d55461971df4
Harley-xk <harley.gb@foxmail.com>
2020-10-10 09:55:44 +0800

    Example for Global Responder configuraction

--- Example/AppDelegate.swift
+++ Example/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Chrysan
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let options = HUDResponder.global.viewOptions
+        options.mainColor = .systemPurple
+        options.hudVisualEffect = UIBlurEffect(style: .extraLight)
+        options.hudCornerRadius = 20
+        
+        HUDResponder.global.register(.circleDots, for: .loading)
+
         return true
     }
 
